0ab7123dd15d8d9c99a1003d58fdc52fdf8b2f09
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-14 21:09:37 +0530

    Added: welcome message

---
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,7 @@
+package com.brigdelabz.gamblingsimulator;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("Wlecome to Gambling Simulator");
+    }
+}
