d00e96acdf6281b4714b0dd643485d0c69d7d760
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-19 22:26:04 +0530

    modified: 20 days of playing

--- src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -5,19 +5,22 @@ public class GamblingSimulator {
     static int STAKE_PER_DAY = 100;
     static int BET_PER_GAME = 1;
     static int win = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program");
-        while(STAKE_PER_DAY > 50 && STAKE_PER_DAY < 150) {
-            int gameCheck = (int) (Math.random() * 10) % 2;
-            if (gameCheck == win) {
-                STAKE_PER_DAY++;
-                System.out.println("you win the won");
+        for (int day = 1; day <= 20; day++) {
+            STAKE_PER_DAY = 100;
 
-            } else {
-                STAKE_PER_DAY--;
-                System.out.println("you lost the game");
+            while (STAKE_PER_DAY > 50 && STAKE_PER_DAY < 150) {
+                int gameCheck = (int) (Math.random() * 10) % 2;
+                if (gameCheck == win) {
+                    STAKE_PER_DAY++;
+
+                } else {
+                    STAKE_PER_DAY--;
+                }
             }
-            System.out.println("stake per day: " + STAKE_PER_DAY);
+            System.out.println("Total money left after 20days : " + STAKE_PER_DAY);
         }
     }
 }
